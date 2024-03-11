a1ad7f01ce9a95d2501090233b7aa811e852b626
Ayush-desaii <110298454+Ayush-desaii@users.noreply.github.com>
2024-03-11 16:08:42 +0530

    intro and button

--- src/pages/Home/Intro.js
+++ src/pages/Home/Intro.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Intro() {
     return (
-        <div className="h-[80vh] bg-primary flex flex-col items-start justify-center gap-8">
+        <div className="h-[80vh] bg-primary flex flex-col items-start justify-center gap-8 py-10">
             <h1 className="text-tertiary">Hi, I'm</h1>
             <h1 className="text-7xl text-secondary font-semibold">Ayush J. Desai</h1>
             <h1 className="text-7xl text-secondary font-semibold">I build Softwares.</h1>
@@ -10,7 +10,7 @@ function Intro() {
                 I am web and AI developer, curruntly studying in third year of computer engineering in sarvajanik 
                 college of engineering and technolgy, surat, gujrat.
             </p>
-
+            <button className="border-2 border-tertiary text-tertiary px-10 py-3 rounded">Get Started</button>
 
         </div>
     )
